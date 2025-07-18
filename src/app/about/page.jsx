2284d661bfbfc1314e610bc9d36b8a89268a8375
TonmoyZohani <tonmoyzohani@gmail.com>
2025-07-18 21:19:37 +0600

    make it nested pages

---
+++ src/app/about/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AboutPage = () => {
+  return (
+    <div>
+      <p className="font-bold text-3xl">About Page</p>
+    </div>
+  );
+};
+
+export default AboutPage;
