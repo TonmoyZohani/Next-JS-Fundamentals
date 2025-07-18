834b8d183ec5660b7dd079173901212652e526c7
TonmoyZohani <tonmoyzohani@gmail.com>
2025-07-18 21:11:14 +0600

    services page created

---
+++ src/app/services/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ServicesPage = () => {
+  return (
+    <div>
+      <p className='font-bold text-3xl'>Service Page</p>
+    </div>
+  )
+}
+
+export default ServicesPage
